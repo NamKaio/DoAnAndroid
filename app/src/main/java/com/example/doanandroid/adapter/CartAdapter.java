71157405dfe8cbc6275825991735fe48556bf466
NamKaio <luonghaonam99@gmail.com>
2019-11-20 23:24:50 +0700

    new feature on cart adapter

--- app/src/main/java/com/example/doanandroid/adapter/CartAdapter.java
+++ app/src/main/java/com/example/doanandroid/adapter/CartAdapter.java
@@ -9,9 +9,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.doanandroid.R;
-import com.example.doanandroid.models.GioHang;
 import com.example.doanandroid.models.HangHoa;
 import com.example.doanandroid.utils.DownloadImage;
 
@@ -19,30 +19,12 @@ import java.util.ArrayList;
 
 public class CartAdapter extends BaseAdapter {
 
-    private ArrayList<GioHang> giohang_list;
+    private ArrayList<HangHoa> giohang_list;
     Context context;
 
-    public CartAdapter(ArrayList<GioHang> giohang_list, Context context) {
+    public CartAdapter(ArrayList<HangHoa> giohang_list, Context context) {
         this.giohang_list = giohang_list;
         this.context = context;
-    }
-
-    private class ClickListener implements View.OnClickListener{
-
-        @Override
-        public void onClick(View v) {
-            switch (v.getId()){
-                case R.id.bt_cart_mines:
-
-                    break;
-                case R.id.bt_cart_plus:
-
-                    break;
-                case R.id.bt_cart_delete:
-
-                    break;
-            }
-        }
     }
 
     @Override
@@ -61,8 +43,8 @@ public class CartAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        MyCartView dataitem;
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final MyCartView dataitem;
         LayoutInflater inflater = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         if (convertView == null) {
             dataitem = new MyCartView();
@@ -79,15 +61,39 @@ public class CartAdapter extends BaseAdapter {
         } else {
             dataitem = (MyCartView) convertView.getTag();
         }
-        new DownloadImage(dataitem.iv_cart_photo).execute(giohang_list.get(position).getSource_photo());
+
+        new DownloadImage(dataitem.iv_cart_photo).execute(giohang_list.get(position).getPhoto());
         dataitem.tv_cart_name.setText(giohang_list.get(position).getName());
         dataitem.tv_cart_kind.setText(giohang_list.get(position).getKind());
         dataitem.tv_cart_cost.setText(giohang_list.get(position).getCost() + "đ");
-        dataitem.et_cart_custom_quantity.setText(giohang_list.get(position).getQuantity() + "");
-        ClickListener listener = new ClickListener();
-        dataitem.bt_cart_mines.setOnClickListener(listener);
-        dataitem.bt_cart_plus.setOnClickListener(listener);
-        dataitem.bt_cart_delete.setOnClickListener(listener);
+        dataitem.et_cart_custom_quantity.setText(1 + "");
+
+        View.OnClickListener onClick = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int quantity = Integer.parseInt(dataitem.et_cart_custom_quantity.getText().toString());
+                switch (v.getId()){
+                    case R.id.bt_cart_mines:
+                        if (quantity > 1) {
+                            dataitem.et_cart_custom_quantity.setText(quantity - 1 + "");
+                        } else {
+                            Toast msg = Toast.makeText(context, "Mặt hàng đã đạt giá trị tối thiểu!", Toast.LENGTH_SHORT);
+                            msg.show();
+                        }
+                        break;
+                    case R.id.bt_cart_plus:
+                        dataitem.et_cart_custom_quantity.setText(quantity + 1 + "");
+                        break;
+                    case R.id.bt_cart_delete:
+
+                        break;
+                }
+            }
+        };
+
+        dataitem.bt_cart_mines.setOnClickListener(onClick);
+        dataitem.bt_cart_plus.setOnClickListener(onClick);
+        dataitem.bt_cart_delete.setOnClickListener(onClick);
         return convertView;
     }
 
